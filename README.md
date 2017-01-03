b9ab745f752594f17832e3814fe65a42bc0ef292
Keisuke Kambara <kambara@takram.com>
2017-01-03 07:11:54 +0000

    model=nmt

--- README.md
+++ README.md
@@ -12,6 +12,6 @@
 
 ## Run
 
-    GOOGLE_API_KEY=xxxxx SLACK_API_TOKEN=xxxxx bundle exec ruby translation-bot.rb
+    TRANSLATE_KEY=xxxxx SLACK_API_TOKEN=xxxxx bundle exec ruby bot.rb
 
 
--- bot.rb
+++ bot.rb
@@ -1,17 +1,16 @@
 require 'slack-ruby-bot'
-require 'google/cloud'
+require 'google/cloud/translate'
 require 'pp'
 
 class TranslationBot < SlackRubyBot::Bot
-  @gcloud    = Google::Cloud.new
-  @translate = @gcloud.translate ENV['GOOGLE_API_KEY']
+  @translate = Google::Cloud::Translate.new
   
   def self.translate(text)
     puts '---'
     puts text
     text = replace(text)
     puts text
-    translated_text = @translate.translate(text, to: 'en').text.to_s
+    translated_text = @translate.translate(text, to: 'en', model: 'nmt').text.to_s
     puts ''
     puts translated_text
     translated_text = restore(translated_text)
@@ -38,19 +37,34 @@ class TranslationBot < SlackRubyBot::Bot
     translated_text
   end
 
+  def self.reply(client, data, username, text)
+    client.web_client.chat_postMessage(
+      channel: data.channel,
+      as_user: true,
+      attachments: [
+        {
+          author_name: username,
+          text: text
+        }
+      ])
+  end
+
   match /\p{Hiragana}|\p{Katakana}|[一-龠々]/ do |client, data, match|
     text = data.text
     username = Slack::Web::Client.new.users_info(user: data.user)[:user][:name]
     if data['subtype'] && data['subtype'] == 'file_share'
       title = data[:file][:title]
-      reply_text = "*#{username}* uploaded: #{translate(title)}"
+      reply_text = "uploaded: #{translate(title)}"
       if data[:file][:initial_comment]
         comment = data[:file][:initial_comment][:comment]
         reply_text += "\n#{translate(comment)}"
       end
-      client.say(text: reply_text, channel: data.channel)
+      reply(client, data, username, reply_text)
+      #client.say(text: reply_text, channel: data.channel)
     else
-      client.say(text: "*#{username}:* #{translate(text)}", channel: data.channel)
+      reply(client, data, username, translate(text))
+      #client.say(text: "*#{username}:* #{translate(text)}",
+      #           channel: data.channel)
     end
   end
 end
